5c8045707cb4c33b4ff639d6a6df863383667859
GargulecMizerion <xmaszekx@gmail.com>
2024-11-13 20:56:30 +0100

    Utworzenie ekranu startowego

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,37 +1,34 @@
-import {StyleSheet, Text, View} from 'react-native'
-import React, {useEffect} from 'react'
-import {Slot, SplashScreen, Stack} from "expo-router";
+import { StyleSheet, Text, View } from "react-native";
+import React, { useEffect } from "react";
+import { Slot, SplashScreen, Stack } from "expo-router";
 import { useFonts } from "expo-font";
 import "../global.css";
-import TabsLayout from "../app/(tabs)/_layout.jsx"
+import TabsLayout from "../app/(tabs)/_layout.jsx";
+import Home from "@/app/(tabs)/home";
 
 SplashScreen.preventAutoHideAsync();
 
-
 const RootLayout = () => {
-    const [fontsLoaded, error] = useFonts( {
-        "Poppins-Black": require("../assets/fonts/Poppins-Black.ttf"),
-    })
+  const [fontsLoaded, error] = useFonts({
+    "Poppins-Black": require("../assets/fonts/Poppins-Black.ttf"),
+  });
 
+  useEffect(() => {
+    if (error) throw error;
 
-    useEffect(() => {
-        if(error) throw error;
+    if (fontsLoaded) SplashScreen.hideAsync();
+  }, [fontsLoaded, error]);
 
-        if(fontsLoaded) SplashScreen.hideAsync();
+  if (!fontsLoaded && !error) return null;
 
-    },[fontsLoaded, error]);
+  return (
+    <Stack>
+      {/*<Stack.Screen name="search" options={{ headerShown: false }} />*/}
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
 
-    if(!fontsLoaded && !error) return null;
-
-
-    return (
-        <Stack>
-            <Stack.Screen name="(tabs)" options={{headerShown: false}}/>
-
-
-            {/* Pierwszy w Stacku okresla ekran startowy   */}
-            {/*<Stack.Screen name="profile" />*/}
-        </Stack>
-    )
-}
-export default RootLayout
+      {/* Pierwszy w Stacku okresla ekran startowy   */}
+      {/*<Stack.Screen name="profile" />*/}
+    </Stack>
+  );
+};
+export default RootLayout;
--- app/index.jsx
+++ app/index.jsx
@@ -1,18 +1,23 @@
 import { Stack } from "expo-router";
-import { Text, View } from "react-native";
+import { Image, ScrollView, Text, View } from "react-native";
 import { StatusBar } from "react-native";
 import { Link } from "expo-router";
+import { SafeAreaView } from "react-native-safe-area-context";
 import "../global.css";
+import { images } from "../constants";
 
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-4xl font-extrabold">Aora!</Text>
-      <StatusBar backgroundColor="blue" style="auto" />
-      <Link href={"/home"} style={{ color: "blue" }}>
-        {" "}
-        Go To Home
-      </Link>
-    </View>
+    <SafeAreaView className="bg-primary h-full">
+      <ScrollView contentContainerStyle={{ height: "100%" }}>
+        <View className="w-full justify-center items-center h-full px-4">
+          <Image
+            source={images.logo}
+            className="w-[130px] h-[84px]"
+            resizeMode={"contain"}
+          />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
